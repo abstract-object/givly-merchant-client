90511e7806fcb9a937a26b31d6931fadaf3d35b7
Jonathan Liu <jxliu95@gmail.com>
2019-07-31 18:02:31 +0000

    button styling

--- src/Components/Dashboard/SearchBar.js
+++ src/Components/Dashboard/SearchBar.js
@@ -1,17 +1,22 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class SearchBar extends Component {
   render() {
     return (
       <article id="search">
-        <input className="searchBar" placeholder="Search Item" onKeyDown={this.search}/>
+        <div className="search-container">
+          <div className="search-group v2">
+            <input className="search-input inactive searchBar" type="text" placeholder="I'm looking for..." onKeyDown={this.search}/>
+            <button className="button button-brand-primary button-search">Search</button>
+          </div>
+        </div>
       </article>
     );
   };
 
   search = event => {
     let searchTerm = event.target.value;
-    
+
     if (event.key === "Enter") {
       if (searchTerm) {
         this.props.search(searchTerm);
